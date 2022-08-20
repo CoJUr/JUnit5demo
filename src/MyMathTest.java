fc4d463027d47c24ec02373177ecebe95e361124
“Cory <coryurton@gmail.com>
2022-08-20 17:18:03 -0500

    switching to mockito

--- src/MyMathTest.java
+++ src/MyMathTest.java
@@ -1,6 +1,4 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,15 +12,7 @@ import static org.junit.Assert.assertEquals;
 public class MyMathTest { 
 
     //MyMath.sum      initially call it with a simple imput, like 3 nums, and it will return 6
-    @Test
-    public void sum_with3numbers() {
-        int[] numbers = {1, 2, 3};
-        int expected = 6;
-        int actual = MyMath.sum(numbers);
 
-        assertEquals(expected, actual);
-
-    }
     @Test
     public void sum_with1number() {
         int[] numbers = { 2};
@@ -34,14 +24,42 @@ public class MyMathTest {
     }
 
 
+    MyMath myMath = new MyMath();
+
 
 @Before
-public void before() throws Exception { 
-} 
+public void before() throws Exception {
+    System.out.println("before");
+}
+
+    @Test
+    public void sum_with3numbers() {
+        int[] numbers = {1, 2, 3};
+        int expected = 6;
+        int actual = MyMath.sum(numbers);
+
+        assertEquals(expected, actual);
+
+    }
+
 
 @After
-public void after() throws Exception { 
-} 
+public void after() throws Exception {
+    System.out.println("after");
+    //cleanup goes here
+}
+
+@BeforeClass
+public static void beforeClass() throws Exception {
+    System.out.println("beforeClass");
+    //establish a connection to the database, init some data, etc
+}
+
+@AfterClass
+public static void afterClass() throws Exception {
+    System.out.println("afterClass");
+    // close the connection to the database, etc
+}
 
 /** 
 * 
